0483aae87f5862a09c98452a1dca00693dded1ec
Weslley Barbosa <weslleyng@hotmail.com>
2026-07-15 11:17:41 -0400

    ajuste hero

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { WaService } from '../../shared/wa.service';
 
 @Component({
   selector: 'app-hero',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
-export class HeroComponent {}
+export class HeroComponent {
+  protected readonly waHref = inject(WaService).urlFor(
+    'Olá, quero começar um diagnóstico com a Plug Sustentável',
+  );
+}
